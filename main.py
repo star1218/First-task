5d55a930f87a1d3d94ee4e7444e897d1eeed4602
igork <igorkaaristocrat@gmail.com>
2024-02-11 22:16:59 +0200

    implemented first example

--- main.py
+++ main.py
@@ -1,40 +1,40 @@
-# n1=int(input("Ведите число:")) #Домашка задание 1
-# n2=int(input("Ведите число:"))
-# n3=int(input("Ведите число:"))
-# operation=input("Выберите Операцию Максимум или Минимум или Среднее:")
-# if operation=="Максимум":
-#     if n1 >= n2:
-#         if n1 >= n3:  #находим максимум
-#             result = n1
-#         elif n2 >= n1 and n2 >= n3:
-#             result = n2
-#         else:
-#             result = n3
-#             print("Максимум:",result)
-#     elif n2 >= n1 and n2 >= n3:
-#         result = n2
-#     else:
-#         result = n3
-#         print("Максимум:",result)
-# elif operation =="Минимум":
-#     if n1 <= n2:
-#         if n1 <= n3: #находим минимум
-#             result = n1
-#         elif n2 <= n1 and n2 <= n3:
-#             result = n2
-#         else:
-#             result = n3
-#             print("Минимум",result)
-#     elif n2 <= n1 and n2 <= n3:
-#         result = n2
-#     else:
-#         result = n3
-#         print("Минимум",result)
-# elif operation =="Среднее":
-#     result=(n1+n2+n3)/3 #Находим Среднее арифметическое по формуле
-#     print("Среднее",result)
-# else:
-#     print("Ошибка")
+n1=int(input("Ведите число:")) #Домашка задание 1
+n2=int(input("Ведите число:"))
+n3=int(input("Ведите число:"))
+operation=input("Выберите Операцию Максимум или Минимум или Среднее:")
+if operation=="Максимум":
+    if n1 >= n2:
+        if n1 >= n3:  #находим максимум
+            result = n1
+        elif n2 >= n1 and n2 >= n3:
+            result = n2
+        else:
+            result = n3
+            print("Максимум:",result)
+    elif n2 >= n1 and n2 >= n3:
+        result = n2
+    else:
+        result = n3
+        print("Максимум:",result)
+elif operation =="Минимум":
+    if n1 <= n2:
+        if n1 <= n3: #находим минимум
+            result = n1
+        elif n2 <= n1 and n2 <= n3:
+            result = n2
+        else:
+            result = n3
+            print("Минимум",result)
+    elif n2 <= n1 and n2 <= n3:
+        result = n2
+    else:
+        result = n3
+        print("Минимум",result)
+elif operation =="Среднее":
+    result=(n1+n2+n3)/3 #Находим Среднее арифметическое по формуле
+    print("Среднее",result)
+else:
+    print("Ошибка")
 #
 #
 # meters=float(input("Ведите число:")) #Домашка задание 2
